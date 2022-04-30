b9ea36f3363dcee59b46e4b012067b178b196283
Nymeria <dev@nymeria.io>
2022-04-30 07:14:21 -0700

    Cleaned up the Enrich API

--- api.go
+++ api.go
@@ -15,6 +15,15 @@ var (
 	ErrInvalidRequest = errors.New(`error: the request looks invalid or malformed`)
 )
 
+// EnrichParams are the parameters used for the enrichment look up. One or more
+// can be supplied to Enrich.
+type EnrichParams struct {
+	URL        string                 `json:"url"`
+	Email      string                 `json:"email"`
+	Identifier string                 `json:"identifier"`
+	Custom     map[string]interface{} `json:"custom,omitempty"`
+}
+
 // Enrichment is a response from the enrichment API. The results of the enrich
 // call, if successful contains a person's information based on one or more of
 // the query parameters.
@@ -22,98 +31,44 @@ type Enrichment struct {
 	Status           string `json:"status"`
 	DeveloperMessage string `json:"developer_message,omitempty"`
 
-	Meta struct {
-		URL        string `json:"url,omitempty"`
-		Identifier string `json:"identifier,omitempty"`
-	} `json:"meta"`
+	Meta EnrichParams `json:"meta"`
 
 	Usage struct {
 		Used  int `json:"used"`
 		Limit int `json:"limit"`
 	} `json:"usage"`
 
-	Data struct {
-		Bio struct {
-			FirstName      string `json:"first_name,omitempty"`
-			LastName       string `json:"last_name,omitempty"`
-			Company        string `json:"company,omitempty"`
-			CompanyWebsite string `json:"company_website,omitempty"`
-		} `json:"bio,omitempty"`
-
-		Emails []struct {
-			Type    string `json:"type,omitempty"`
-			Name    string `json:"name,omitempty"`
-			Domain  string `json:"domain,omitempty"`
-			Address string `json:"address,omitempty"`
-		} `json:"emails,omitempty"`
-
-		PhoneNumbers []struct {
-			Number string `json:"number,omitempty"`
-		} `json:"phone_numbers,omitempty"`
-
-		Social []struct {
-			Type string `json:"type,omitempty"`
-			ID   string `json:"id,omitempty"`
-			URL  string `json:"url,omitempty"`
-		} `json:"social,omitempty"`
-	} `json:"data"`
+	Data Person `json:"data"`
 }
 
-// BulkEnrichment is an aggregate response of Enrichments.
-type BulkEnrichment struct {
-	Status           string `json:"status"`
-	DeveloperMessage string `json:"developer_message,omitempty"`
-
-	Usage struct {
-		Used  int `json:"used"`
-		Limit int `json:"limit"`
-	} `json:"usage"`
-
-	Data []struct {
-		Meta struct {
-			Custom     map[string]interface{} `json:"custom,omitempty"`
-			URL        string                 `json:"url,omitempty"`
-			Identifier string                 `json:"identifier,omitempty"`
-		} `json:"meta"`
-
-		Result struct {
-			Bio struct {
-				FirstName      string `json:"first_name,omitempty"`
-				LastName       string `json:"last_name,omitempty"`
-				Company        string `json:"company,omitempty"`
-				CompanyWebsite string `json:"company_website,omitempty"`
-			} `json:"bio,omitempty"`
-
-			Emails []struct {
-				Type    string `json:"type,omitempty"`
-				Name    string `json:"name,omitempty"`
-				Domain  string `json:"domain,omitempty"`
-				Address string `json:"address,omitempty"`
-			} `json:"emails,omitempty"`
-
-			PhoneNumbers []struct {
-				Number string `json:"number,omitempty"`
-			} `json:"phone_numbers,omitempty"`
-
-			Social []struct {
-				Type string `json:"type,omitempty"`
-				ID   string `json:"id,omitempty"`
-				URL  string `json:"url,omitempty"`
-			} `json:"social,omitempty"`
-		} `json:"result"`
-	} `json:"data"`
-}
-
-// Person represents a single person query.
+// Person is a successful enrichment.
 type Person struct {
-	Custom     map[string]interface{} `json:"custom,omitempty"`
-	URL        string                 `json:"url,omitempty"`
-	Identifier string                 `json:"identifier,omitempty"`
-}
+	Bio struct {
+		FirstName      string `json:"first_name,omitempty"`
+		LastName       string `json:"last_name,omitempty"`
+		Title          string `json:"title,omitempty"`
+		Location       string `json:"location,omitempty"`
+		Country        string `json:"country,omitempty"`
+		Company        string `json:"company,omitempty"`
+		CompanyWebsite string `json:"company_website,omitempty"`
+	} `json:"bio,omitempty"`
 
-// BulkPayload is the payload of urls and other query parameters.
-type BulkPayload struct {
-	People []Person `json:"people"`
+	Emails []struct {
+		Type    string `json:"type,omitempty"`
+		Name    string `json:"name,omitempty"`
+		Domain  string `json:"domain,omitempty"`
+		Address string `json:"address,omitempty"`
+	} `json:"emails,omitempty"`
+
+	PhoneNumbers []struct {
+		Number string `json:"number,omitempty"`
+	} `json:"phone_numbers,omitempty"`
+
+	Social []struct {
+		Type string `json:"type,omitempty"`
+		ID   string `json:"id,omitempty"`
+		URL  string `json:"url,omitempty"`
+	} `json:"social,omitempty"`
 }
 
 //
@@ -175,58 +130,46 @@ func Verify(email string) (*Verification, error) {
 	return &response, nil
 }
 
-// Enrich takes a url as a first parameter.
-func Enrich(u string) (*Enrichment, error) {
-	req, err := request("GET", fmt.Sprintf("/enrich?url=%s", url.QueryEscape(u)), nil)
+// Enrich takes one or more EnrichParams and will return an enrichment result
+// for each enrichment.
+func Enrich(args ...EnrichParams) ([]Enrichment, error) {
+	var es []Enrichment
 
-	if err != nil {
-		return nil, err
+	switch len(args) {
+	case 0: /* error */
+		return es, ErrInvalidRequest
+	case 1: /* single enrichment*/
+		enrichment, err := enrich(args[0])
+
+		if err != nil {
+			return es, err
+		}
+
+		es = append(es, *enrichment)
+
+		return es, nil
+	default: /* bulk enrichment*/
+		es, err := bulk(args...)
+
+		if err != nil {
+			return es, err
+		}
+
+		return es, nil
 	}
 
-	resp, err := client.Do(req)
-
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-
-	bs, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		return nil, err
-	}
-
-	var response Enrichment
-
-	if err := json.Unmarshal(bs, &response); err != nil {
-		return nil, err
-	}
-
-	if response.Status != "success" {
-		return nil, ErrInvalidRequest
-	}
-
-	return &response, nil
+	return es, ErrInvalidRequest
 }
 
-// BulkEnrich takes one or more custom queries and returns zero or more matches.
-func BulkEnrich(u ...string) (*BulkEnrichment, error) {
-	var payload BulkPayload
-
-	for _, iu := range u {
-		payload.People = append(payload.People, Person{
-			URL: iu,
-		})
-	}
-
-	bs, err := json.Marshal(payload)
+// Performs a single enrichment.
+func enrich(params EnrichParams) (*Enrichment, error) {
+	bs, err := json.Marshal(params)
 
 	if err != nil {
 		return nil, err
 	}
 
-	req, err := request("POST", "/bulk-enrich", bytes.NewBuffer(bs))
+	req, err := request("POST", "/enrich", bytes.NewBuffer(bs))
 
 	if err != nil {
 		return nil, err
@@ -246,7 +189,7 @@ func BulkEnrich(u ...string) (*BulkEnrichment, error) {
 		return nil, err
 	}
 
-	var response BulkEnrichment
+	var response Enrichment
 
 	if err := json.Unmarshal(bs, &response); err != nil {
 		return nil, err
@@ -257,4 +200,85 @@ func BulkEnrich(u ...string) (*BulkEnrichment, error) {
 	}
 
 	return &response, nil
+}
+
+// Performs a bulk enrichment and converts the results into a slice of
+// enrichments.
+func bulk(params ...EnrichParams) ([]Enrichment, error) {
+	var es []Enrichment
+
+	if len(params) == 0 {
+		return es, ErrInvalidRequest
+	}
+
+	type BulkEnrichment struct {
+		Status           string `json:"status"`
+		DeveloperMessage string `json:"developer_message,omitempty"`
+
+		Usage struct {
+			Used  int `json:"used"`
+			Limit int `json:"limit"`
+		} `json:"usage"`
+
+		Data []struct {
+			Meta   EnrichParams `json:"meta"`
+			Result Person       `json:"result"`
+		} `json:"data"`
+	}
+
+	type BulkPayload struct {
+		People []EnrichParams `json:"people"`
+	}
+
+	var payload BulkPayload
+
+	payload.People = append(payload.People, params...)
+
+	bs, err := json.Marshal(payload)
+
+	if err != nil {
+		return es, err
+	}
+
+	req, err := request("POST", "/bulk-enrich", bytes.NewBuffer(bs))
+
+	if err != nil {
+		return es, err
+	}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return es, err
+	}
+
+	defer resp.Body.Close()
+
+	bs, err = ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return es, err
+	}
+
+	var response BulkEnrichment
+
+	if err := json.Unmarshal(bs, &response); err != nil {
+		return es, err
+	}
+
+	if response.Status != "success" {
+		return es, ErrInvalidRequest
+	}
+
+	for _, result := range response.Data {
+		es = append(es, Enrichment{
+			Status:           response.Status,
+			DeveloperMessage: response.DeveloperMessage,
+			Usage:            response.Usage,
+			Meta:             result.Meta,
+			Data:             result.Result,
+		})
+	}
+
+	return es, nil
 }
