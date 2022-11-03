b01ddc961ed4cd96a2d68b493e1b5ed4e4c462dc
Corey Prophitt <git@prophitt.me>
2022-11-03 06:23:07 -0700

    Added bulk person enrich

--- person/enrich.go
+++ person/enrich.go
@@ -1,6 +1,7 @@
 package person
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -10,6 +11,11 @@ import (
 	"git.nymeria.io/nymeria.go"
 	"git.nymeria.io/nymeria.go/internal/api"
 )
+
+type BulkEnrichParams struct {
+	Params   interface{} `json:"params"`
+	MetaData interface{} `json:"metadata"`
+}
 
 type EnrichParams struct {
 	Profile string
@@ -77,4 +83,63 @@ func Enrich(params EnrichParams) (*Person, error) {
 	}
 
 	return &response.Data, nil
+}
+
+func BulkEnrich(params ...BulkEnrichParams) ([]Person, error) {
+	if len(params) == 0 {
+		return nil, nymeria.ErrInvalidParameters
+	}
+
+	bs, err := json.Marshal(map[string]interface{}{
+		"requests": params,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := api.Request("POST", "/person/enrich/bulk", bytes.NewBuffer(bs))
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := api.Client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		if e, ok := nymeria.ErrMap[resp.StatusCode]; ok {
+			return nil, e
+		}
+
+		return nil, nymeria.ErrServerError
+	}
+
+	defer resp.Body.Close()
+
+	bs, err = ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var response []struct {
+		Status int    `json:"status"`
+		Data   Person `json:"data"`
+	}
+
+	if err := json.Unmarshal(bs, &response); err != nil {
+		return nil, err
+	}
+
+	var records []Person
+
+	for _, v := range response {
+		records = append(records, v.Data)
+	}
+
+	return records, nil
 }
