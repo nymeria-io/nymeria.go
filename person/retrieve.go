8e3eefdd2790c5f3bde1912728f48c2bd196ffe9
Corey Prophitt <git@prophitt.me>
2022-11-03 07:32:25 -0700

    Added bulk retrieve

--- person/retrieve.go
+++ person/retrieve.go
@@ -1,6 +1,7 @@
 package person
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -9,6 +10,11 @@ import (
 	"git.nymeria.io/nymeria.go"
 	"git.nymeria.io/nymeria.go/internal/api"
 )
+
+type BulkRetrieveParams struct {
+	ID       string                 `json:"id"`
+	MetaData map[string]interface{} `json:"metadata"`
+}
 
 func Retrieve(id string) (*Person, error) {
 	if len(id) == 0 {
@@ -53,4 +59,64 @@ func Retrieve(id string) (*Person, error) {
 	}
 
 	return &response.Data, nil
+}
+
+func BulkRetrieve(params ...BulkRetrieveParams) ([]Person, error) {
+	if len(params) == 0 {
+		return nil, nymeria.ErrInvalidParameters
+	}
+
+	bs, err := json.Marshal(map[string]interface{}{
+		"requests": params,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := api.Request("POST", "/person/retrieve/bulk", bytes.NewBuffer(bs))
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := api.Client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		if e, ok := nymeria.ErrMap[resp.StatusCode]; ok {
+			return nil, e
+		}
+
+		return nil, nymeria.ErrServerError
+	}
+
+	defer resp.Body.Close()
+
+	bs, err = ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var response []struct {
+		Status   int                    `json:"status"`
+		MetaData map[string]interface{} `json:"metadata"`
+		Data     Person                 `json:"data"`
+	}
+
+	if err := json.Unmarshal(bs, &response); err != nil {
+		return nil, err
+	}
+
+	var records []Person
+
+	for _, v := range response {
+		records = append(records, v.Data)
+	}
+
+	return records, nil
 }
