e160eae88bb5835f026ab847878401e842d5d0cf
Corey Prophitt <git@prophitt.me>
2025-02-03 09:46:36 -0800

    Fixed company search

--- company/search.go
+++ company/search.go
@@ -3,31 +3,60 @@ package company
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/nymeriaio/nymeria.go"
 	"github.com/nymeriaio/nymeria.go/internal/api"
 )
 
 type SearchParams struct {
-	Query string
-	Size  int /* how many records to retrieve starting at From (default: 0) */
-	From  int /* from which record to start */
+	Name     string
+	Location string
+	Country  string
+	Industry string
+	Size     string
+	Limit    int /* how many records to retrieve starting (default: 0) */
+	Offset   int /* from which record to start */
 }
 
 func (s SearchParams) Invalid() bool {
-	return len(s.Query) == 0
+	return len(s.Name) == 0 && len(s.Location) == 0 && len(s.Country) == 0 && len(s.Industry) == 0 && len(s.Size) == 0
 }
 
 func (s SearchParams) URL() string {
-	return fmt.Sprintf(
-		"query=%s&size=%s&from=%s",
-		url.QueryEscape(s.Query),
-		url.QueryEscape(fmt.Sprintf("%d", s.Size)),
-		url.QueryEscape(fmt.Sprintf("%d", s.From)),
-	)
+	if s.Limit <= 0 || s.Limit > 100 {
+		s.Limit = 10
+	}
+
+	var query strings.Builder
+
+	query.WriteString(fmt.Sprintf("limit=%d", s.Limit))
+	query.WriteString(fmt.Sprintf("&offset=%d", s.Offset))
+
+	if len(s.Name) > 0 {
+		query.WriteString(fmt.Sprintf("&name=%s", url.QueryEscape(s.Name)))
+	}
+
+	if len(s.Size) > 0 {
+		query.WriteString(fmt.Sprintf("&size=%s", url.QueryEscape(s.Size)))
+	}
+
+	if len(s.Location) > 0 {
+		query.WriteString(fmt.Sprintf("&location=%s", url.QueryEscape(s.Location)))
+	}
+
+	if len(s.Country) > 0 {
+		query.WriteString(fmt.Sprintf("&country=%s", url.QueryEscape(s.Country)))
+	}
+
+	if len(s.Industry) > 0 {
+		query.WriteString(fmt.Sprintf("&industry=%s", url.QueryEscape(s.Industry)))
+	}
+
+	return query.String()
 }
 
 func Search(params SearchParams) ([]Company, error) {
@@ -57,7 +86,7 @@ func Search(params SearchParams) ([]Company, error) {
 
 	defer resp.Body.Close()
 
-	bs, err := ioutil.ReadAll(resp.Body)
+	bs, err := io.ReadAll(resp.Body)
 
 	if err != nil {
 		return nil, err
